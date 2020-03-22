685b236a0b549f7c1dad862d3548b979a2c70e30
Michael Fridrikhson <fridrihson.1701@gmail.com>
2020-03-22 23:28:48 +0200

    fix: Welcome message typo

--- config/bot.config.js
+++ config/bot.config.js
@@ -2,5 +2,5 @@ require('dotenv').config();
 
 module.exports = {
   token: process.env.TELEGRAM_TOKEN,
-  welcomeMessage: 'Welcome!\n\nI can retrieve you any *Minecraft* crafting recipe you would ask it.\n\nJust send me the name of an item you want to look up!'
+  welcomeMessage: 'Welcome!\n\nI can retrieve you any *Minecraft* crafting recipe you would ask for.\n\nJust send me the name of an item you want to look up!'
 };
