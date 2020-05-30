1cdd54c01614b8a65902969f4842b16803c96727
Michael Fridrikhson <fridrihson.1701@gmail.com>
2020-05-31 01:48:29 +0300

    Add stats collection using Dashbot

--- handlers/recipe.handler.js
+++ handlers/recipe.handler.js
@@ -1,16 +1,42 @@
+const dashbot = require('dashbot')('WjxIknKldXiynnkKfQo0K3gTaZIYdrYe2DYgLAae').universal;
+
 const { getItemImage } = require('../repositories/item.repository');
+const { getFileLink } = require('../helpers/telegramFileLink.helper');
 
 const recipeHandler = async ctx => {
+  dashbot.logIncoming({
+    text: ctx.message.text,
+    userId: ctx.chat.id.toString()
+  });
+
   try {
+    let answerMessage;
     const { image, animated } = await getItemImage(ctx.message.text);
 
     if (animated) {
-      ctx.replyWithAnimation({ source: image });
+      answerMessage = await ctx.replyWithAnimation({ source: image });
     } else {
-      ctx.replyWithPhoto({ source: image });
+      answerMessage = await ctx.replyWithPhoto({ source: image });
     }
+
+    const fileId = animated ? answerMessage.animation.file_id : answerMessage.photo[0].file_id;
+    const fileUrl = await getFileLink(fileId);
+
+    dashbot.logOutgoing({
+      text: '',
+      userId: answerMessage.chat.id.toString(),
+      images: [{
+        url: fileUrl
+      }]
+    });
   } catch (error) {
-    ctx.reply('I can\'t find that item :(');
+    const answerMessage = await ctx.reply('I can\'t find that item :(');
+    console.log(error);
+
+    dashbot.logOutgoing({
+      text: answerMessage.text,
+      userId: answerMessage.chat.id.toString()
+    });
   }
 };
 
